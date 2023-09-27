c6dca48dfd7570939c66f97c46869863b80bfab0
angelpilled <angelpilleddev@gmail.com>
2023-09-27 19:16:09 +0200

    Fibonacci Sequence Program added

--- GPT-4/6_Fibonacci.c
+++ GPT-4/6_Fibonacci.c
@@ -1,23 +1,33 @@
-// 6. Fibonacci
-// 0 1 1 2 3 5 8 13 etc.
+// 6. Fibonacci Sequence
+// https://github.com/angelpilled/AngelC/tree/master/GPT-4
 
 #include <stdio.h>
 
-int FibonacciFunc(int showThisMany) {
-
-
-	return 0;	
-}
-
-int main (void) {
+int FibonacciSequence(int t) {
 	
-	int showThisMany;
-
-	printf("How many Fibonacci sequences would you like to see?: ");
-	scanf("%d", &showThisMany);
-
+	int t1 = 0, t2 = 1, sum;
 	
+	for (int iteration = 1; iteration < t; iteration++) {
+		sum = t1 + t2;
+		t1 = t2;
+		t2 = sum;
+		printf("%d ", sum);
+	}
 
 	return 0;
 
+}
+
+int main (void) {
+
+	int t;
+
+	printf("Number of terms I want to display: ");
+	scanf("%d", &t);
+
+	printf("The Fibonacci sequence with %d terms: %d ", t, 0);
+	FibonacciSequence(t);
+
+
+	return 0;
 }
